03b957411df5bfbdd8e0daaba44947d024447fbe
sandrbuz <sandrbuz@mail.ru>
2021-01-19 15:35:04 +0300

    Advanced calculator js

--- main.js
+++ main.js
@@ -1,59 +1,63 @@
-var btnPlus = document.getElementById('buttonPlus');
-var btnMinus = document.getElementById('buttonMinus');
-var btnMult = document.getElementById('buttonMult');
-var btnDivide = document.getElementById('buttonDivide');
+// let btnPlus = document.getElementById('buttonPlus');
+// let btnMinus = document.getElementById('buttonMinus');
+// let btnMult = document.getElementById('buttonMult');
+// let btnDivide = document.getElementById('buttonDivide');
 
 
-function onButtonPlusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
+
+
+var operationButtons = document.getElementsByClassName('operation-button');
+// var operationButtons = [btnPlus, btnMinus, btnMult, btnDivide];
+
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+
+
+function makeOperation(operationCode) {
 
     var number1 = Number(input1.value);
     var number2 = Number(input2.value);
+    // что делать
+    if (operationCode === '+') {
+        var result = number1 + number2;
+    } else if (operationCode === '-') {
+        var result = number1 - number2;
+    } else if (operationCode === '*') {
+        var result = number1 * number2;
+    } else if (operationCode === '/') {
+        var result = number1 / number2;
+    } else {
+        window.alert('Operation is unknown');
+    }
 
-    var summ = number1 + number2;
-    window.alert(summ);
+    window.alert(result);
 }
 
-function onButtonMinusClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 - number2;
-    window.alert(summ);
-}
-function onButtonMultiplyClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 * number2;
-    window.alert(summ);
-}
-function onButtonDivideClick() {
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-
-    var summ = number1 / number2;
-    window.alert(summ);
+function onOperationButtonClick(eventObject) {
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
+    //  с кем делать
 }
 
 
 
 
-btnPlus.addEventListener('click', onButtonPlusClick);
-btnMinus.addEventListener('click', onButtonMinusClick);
-btnMult.addEventListener('click', onButtonMultiplyClick);
-btnDivide.addEventListener('click', onButtonDivideClick);
 
+
+
+for (var i = 0; i < operationButtons.length; i++) {
+    var button = operationButtons[i];
+    button.addEventListener('click', onOperationButtonClick);
+    // сколько раз делать (4)
+}
+
+
+            // замена for
+// operationButtons[0].addEventListener('click', onOperationButtonClick);
+// operationButtons[1].addEventListener('click', onOperationButtonClick);
+// operationButtons[2].addEventListener('click', onOperationButtonClick);
+// operationButtons[3].addEventListener('click', onOperationButtonClick);
 
 
 
